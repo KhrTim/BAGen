35ec13289cf67ed5814075854ec5be7456e81fee
Timur Khairulov <khairulov00@gmail.com>
2025-02-16 11:47:12 +0900

    - Replace print with logging - add copy to media/results directory - add video convertion using vp09 codec

--- utils/create_animation.py
+++ utils/create_animation.py
@@ -1,21 +1,40 @@
 import os
 import subprocess
-
+import logging
 import PIL
+from .common import (
+    copy_image_to_subdirectory,
+    get_list_of_files,
+    delete_files_in_directory,
+)
+import shutil
+from .video_converter import convert_video
 
-from .common import copy_image_to_subdirectory, get_list_of_files
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 CINEMO_DIR = os.path.join("submodules", "Cinemo")
 
 
 def create_cinemo_visualisation(
-    prompt: str, image_asset: PIL.Image, visualization_intensity: int, num_frames: int
+    prompt: str,
+    image_asset: PIL.Image,
+    visualization_intensity: int,
+    num_animation_steps: int,
+    video_len_frames: int,
+    save_path: os.PathLike,
 ) -> os.PathLike:
+    logging.debug("++++++++ ASSET ++++++++")
+    logging.debug(image_asset)
     cinemo_asses_dir = os.path.join(CINEMO_DIR, "animated_images")
+    delete_files_in_directory(cinemo_asses_dir)
     filename = copy_image_to_subdirectory(image_asset, cinemo_asses_dir)
-    print(filename)
+    logging.debug(filename)
+    visualization_dir = os.path.join(CINEMO_DIR, "sample_videos")
+    delete_files_in_directory(visualization_dir)
 
-    subprocess.call(
+    subprocess.run(
         [
             "python",
             "pipelines/animation.py",
@@ -25,15 +44,23 @@ def create_cinemo_visualisation(
             f"'{prompt}'",
             "--intensity",
             str(visualization_intensity),
-            "--frames",
-            str(num_frames),
+            "--num_sampling_steps",
+            str(num_animation_steps),
+            "--video_length",
+            str(video_len_frames),
         ],
         cwd="./submodules/Cinemo/",
     )
 
-    visualization_dir = os.path.join(CINEMO_DIR, "sample_videos")
-    visualzation_filename = get_list_of_files(visualization_dir)[0]
-    print(visualization_dir)
-    print(visualzation_filename)
+    logging.debug("Subprocess fininsh")
 
-    return visualzation_filename
+    visualzation_filename = get_list_of_files(visualization_dir)[0]
+    convert_video(visualzation_filename, save_path)
+    shutil.copy(visualzation_filename, save_path.removesuffix(".webm")+".mp4")
+    logging.info(f"File saved as {visualzation_filename}")
+    logging.info(f"File copied to {save_path}")
+
+    logging.debug(visualization_dir)
+    logging.debug(visualzation_filename)
+
+    return save_path
